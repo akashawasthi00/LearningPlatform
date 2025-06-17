af485f14eadcf025504d1f0555318e7072ac53ce
akashawasthi <akash.awasthi@axtrum.com>
2025-06-17 17:43:41 +0530

    updated

---
+++ PublishOutput/Assets/site.js
@@ -0,0 +1,13 @@
+﻿angular.module('tutorialApp', ['ngSanitize'])
+    .controller('ContentController', function ($scope, $sce) {
+        $scope.content = window.contentData || [];
+        console.log('Content Data in AngularJS:', $scope.content);
+
+        $scope.trustHtml = function (html) {
+            return $sce.trustAsHtml(html);
+        };
+
+        if (!$scope.content || $scope.content.length === 0) {
+            console.warn('No content data available');
+        }
+    });
